169b09e43c41547f9c05c8a17b9c72a6126e1154
Vanhung0611 <vanhungvn741@gmail.com>
2026-02-03 18:00:46 +0700

    Create OwnerMetadata.cs

---
+++ Vehicle_Inspection/Models/Metadata/OwnerMetadata.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicle_Inspection.Models.Metadata
+{
+    [ModelMetadataType(typeof(OwnerMetadata))]
+
+    public partial class Owner
+    {
+    }
+    public class OwnerMetadata
+    {
+
+    }
+}
